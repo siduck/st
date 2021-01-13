59974f3ee607531caaa2cfd6aab39a0d9f606d3a
siduck76 <siduckk76@protonmail.com>
2021-01-13 21:01:19 +0530

    n

--- config.h
+++ config.h
@@ -7,7 +7,7 @@
  */
 static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
 static char *font2[] = { " JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true" };
-static int borderpx = 9;
+static int borderpx = 0;
 
 /*
  * What program is execed by st depends of these precedence rules:
