49fe0a17d8e2a7d46ff28f2631ca6ddf19d6c048
siduck76 <siduckk76@protonmail.com>
2021-04-06 11:57:40 +0530

    updated xresources colorscheme

--- config.h
+++ config.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true";
-static char *font2[] = { "JetBrainsMono Nerd Font Mono:pixelsize=15:antialias=true:autohint=true" };
+static char *font2[] = { "JetBrainsMono Nerd Font :pixelsize=15:antialias=true:autohint=true" };
 static int borderpx = 0;
 
 /*
