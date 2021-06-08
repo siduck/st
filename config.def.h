f4cd34342913024bc1708b3f795fa7febc2ae95b
TripleK2004 <kowkeerthankumar@gmail.com>
2021-06-08 22:20:08 +0530

    patched dynamic cursor

--- config.def.h
+++ config.def.h
@@ -17,7 +17,7 @@ static int borderpx = 0;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/fish";
+static char *shell = "/bin/sh";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
