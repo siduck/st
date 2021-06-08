99704e1b41d4c5a74365b0ff0e398f4bd898da74
TripleK2004 <kowkeerthankumar@gmail.com>
2021-06-08 23:01:12 +0530

    Revert "patched dynamic cursor"
    This reverts commit f4cd34342913024bc1708b3f795fa7febc2ae95b.

--- config.h
+++ config.h
@@ -17,7 +17,7 @@ static int borderpx = 0;
  * 4: value of shell in /etc/passwd
  * 5: value of shell in config.h
  */
-static char *shell = "/bin/sh";
+static char *shell = "/bin/fish";
 char *utmp = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
