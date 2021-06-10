8b873dbb69c1ef165ef4c21618d87a3be0d8e1c2
siduck76 <siduckk76@protonmail.com>
2021-06-10 08:32:08 +0530

    disable boxdraw for brailie

--- config.h
+++ config.h
@@ -85,7 +85,7 @@ const int boxdraw = 1;
 const int boxdraw_bold = 1;
 
 /* braille (U28XX):  1: render as adjacent "pixels",  0: use font */
-const int boxdraw_braille = 1;
+const int boxdraw_braille = 0;
 
 /*
  * bell volume. It must be a value between -100 and 100. Use 0 for disabling
