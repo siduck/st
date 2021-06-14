b271b22fce14d80ac302d45289e3e77906f451c1
siduck76 <siduckk76@protonmail.com>
2021-06-14 08:47:57 +0530

    hide cursor on idle window

--- x.c
+++ x.c
@@ -1621,7 +1621,7 @@ xdrawcursor(int cx, int cy, Glyph g, int ox, int oy, Glyph og, Line line, int le
 	 * It will restore the ligatures broken by the cursor. */
 	xdrawline(line, 0, oy, len);
 
-	if (IS_SET(MODE_HIDE))
+        if (IS_SET(MODE_HIDE) || !IS_SET(MODE_FOCUSED))
 		return;
 
 	/*
