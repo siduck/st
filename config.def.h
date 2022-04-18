a373e233e86e8bf1268fcee28e2abcd3014db90c
Simon Lehn <48837958+srlehn@users.noreply.github.com>
2022-04-18 16:53:26 +0200

    report sixel capability

--- config.def.h
+++ config.def.h
@@ -29,7 +29,7 @@ char *scroll = NULL;
 char *stty_args = "stty raw pass8 nl -echo -iexten -cstopb 38400";
 
 /* identification sequence returned in DA and DECID */
-char *vtiden = "\033[?6c";
+char *vtiden = "\033[?63;4c";
 
 /* Kerning / character bounding-box multipliers */
 static float cwscale = 1.0;
