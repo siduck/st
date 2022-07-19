eb663a1c296d9d91c41b4be4189a4c411f477c5b
sioodmy <sioodmy@tuta.io>
2022-07-19 20:52:38 +0200

    feat: implement nix flake

---
+++ flake.nix
@@ -0,0 +1,31 @@
+{
+  description = "Flake utils demo";
+
+  inputs.nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in rec {
+        packages = flake-utils.lib.flattenTree {
+          st-snazzy = pkgs.callPackage ./default.nix { };
+        };
+        defaultPackage = packages.st-snazzy;
+        apps.st-snazzy = flake-utils.lib.mkApp { drv = packages.st-snazzy; };
+        defaultApp = apps.st-snazzy;
+        devShell = pkgs.mkShell rec {
+          name = "st-snazzy";
+          packages = with pkgs; [
+            pkgconfig
+            xorg.libX11
+            xorg.libXft
+            fontconfig
+            harfbuzz
+            gd
+            glib
+          ];
+        };
+
+      });
+}
